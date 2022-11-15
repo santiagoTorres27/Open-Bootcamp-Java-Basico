06c28a3ae72f04ec44bec629975c0f61ad32655c
santiagoTorres27 <santiago.torres27@outlook.com>
2022-11-15 23:53:40 +0100

    Java Basico - Ejercicio 1

---
+++ src/main/java/org/example/variables/JavaVariables.java
@@ -0,0 +1,25 @@
+package org.example.variables;
+
+public class JavaVariables {
+    public static void main(String[] args) {
+
+        byte myByte = 1;
+        short myShort = 2;
+        int myInt = 3;
+        long myLong = 4;
+
+        double myDouble = 5.6;
+        float myFloat = 6.7345f;
+        char myChar = 'a';
+
+        System.out.println("Byte: " + myByte);
+        System.out.println("Short: " + myShort);
+        System.out.println("Int: " + myInt);
+        System.out.println("Long: " + myLong);
+        System.out.println("Double: " + myDouble);
+        System.out.println("Float: " + myFloat);
+        System.out.println("Char: " + myChar);
+
+
+    }
+}
